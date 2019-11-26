45925371660ce7e4f2db8dc3010db83016237492
unknown <iwbs3417@gmail.com>
2019-11-27 00:08:15 +0800

    updates

--- train.py
+++ train.py
@@ -37,9 +37,11 @@ if __name__ == "__main__":
   parser.add_argument("--path", help="HDFS path")
   parser.add_argument("--cluster_size", help="number of nodes in the cluster", type=int, default=num_executors)
   parser.add_argument("--tensorboard", help="launch tensorboard process", action="store_true")
+  parser.add_argument("--steps", help="maximum number of steps", type=int, default=1000)
   parser.add_argument("--epochs", help="number of epochs", type=int, default=100)
   parser.add_argument("--batch_size", help="number of records per batch", type=int, default=64)
   parser.add_argument("--mode", help="train|inference", default="train")
+  parser.add_argument("--rdma", help="use rdma connection", default=False)
   args = parser.parse_args()
   print("args:", args)
 
